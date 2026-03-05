6023d6d840e48d5f6e6bc40641cbb6117706a787
bivansir <bivansir@gmail.com>
2026-03-05 15:51:45 +0300

    Initial commit

---
+++ calcservice/src/test/java/ru/neoflex/calcservice/CalcserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.calcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
