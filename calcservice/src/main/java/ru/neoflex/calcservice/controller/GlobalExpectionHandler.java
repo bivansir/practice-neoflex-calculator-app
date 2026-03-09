ebda8c1c111bf3abb266df63e91861b56232c315
bivansir <bivansir@gmail.com>
2026-03-09 11:28:04 +0300

    fix: refactored GlobalExceptionHandler

--- calcservice/src/main/java/ru/neoflex/calcservice/controller/GlobalExpectionHandler.java
+++ calcservice/src/main/java/ru/neoflex/calcservice/controller/GlobalExpectionHandler.java
@@ -1,13 +1,11 @@
 package ru.neoflex.calcservice.controller;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.neoflex.calcservice.service.BusinessValidationException;
@@ -49,12 +47,8 @@ public class GlobalExpectionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(
-            HttpMessageNotReadableException ex) {
-        String details = "Неверный формат JSON";
-        if (ex.getCause() instanceof InvalidFormatException) {
-            details = "Ошибка в формате данных: проверьте типы полей";
-        }
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable() {
+        String details = "Ошибка в формате данных";
         return ResponseEntity.badRequest().body(
                 ErrorResponse.builder()
                         .code("BAD_REQUEST")
@@ -65,23 +59,8 @@ public class GlobalExpectionHandler {
         );
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ErrorResponse> handleMissingParams(
-            MissingServletRequestParameterException ex) {
-
-        return ResponseEntity.badRequest().body(
-                ErrorResponse.builder()
-                        .code("BAD_REQUEST")
-                        .message("Отсутствует обязательный параметр")
-                        .details("Параметр: " + ex.getParameterName())
-                        .timestamp(LocalDateTime.now())
-                        .build()
-        );
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
-
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
                 ErrorResponse.builder()
                         .code("INTERNAL_ERROR")
