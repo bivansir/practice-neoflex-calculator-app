ded00614a1eae995d0060a60c014c07f14b08c5f
bivansir <bivansir@gmail.com>
2026-03-23 15:24:54 +0300

    fix: remove @Data

--- deal/src/main/java/ru/neoflex/deal/entity/Credit.java
+++ deal/src/main/java/ru/neoflex/deal/entity/Credit.java
@@ -12,38 +12,46 @@ import java.util.UUID;
 
 @Entity
 @Builder
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "credit")
 public class Credit {
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "credit_id")
     private UUID creditId;
 
+    @Getter
     @Column(name = "amount", nullable = false)
     private BigDecimal amount;
 
+    @Getter
     @Column(name = "term", nullable = false)
     private Integer term;
 
+    @Getter
     @Column(name = "monthly_payment", nullable = false)
     private BigDecimal monthlyPayment;
 
+    @Getter
     @Column(name = "rate", nullable = false)
     private BigDecimal rate;
 
+    @Getter
     @Column(name = "psk", nullable = false)
     private BigDecimal psk;
 
+    @Getter
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(name = "payment_schedule", nullable = false, columnDefinition = "jsonb")
     private List<PaymentScheduleElementDto> paymentSchedule;
 
+    @Getter
     @Column(name = "insurance_enabled", nullable = false)
     private Boolean isInsuranceEnabled;
 
+    @Getter
     @Column(name = "salary_client")
     private Boolean isSalaryClient;
 
