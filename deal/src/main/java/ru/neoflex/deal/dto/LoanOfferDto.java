385df866f9da070c309c804a1b055bcf8c22dfe9
bivansir <bivansir@gmail.com>
2026-03-23 19:42:06 +0300

    refactor: add schema for LoanOfferDto

--- deal/src/main/java/ru/neoflex/deal/dto/LoanOfferDto.java
+++ deal/src/main/java/ru/neoflex/deal/dto/LoanOfferDto.java
@@ -1,5 +1,6 @@
 package ru.neoflex.deal.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,23 +19,30 @@ public class LoanOfferDto {
     private UUID statementId;
 
     @NotNull
+    @Schema(example = "10000000")
     private BigDecimal requestedAmount;
 
     @NotNull
+    @Schema(example = "1200000")
     private BigDecimal totalAmount;
 
     @NotNull
+    @Schema(example = "12")
     private Integer term;
 
     @NotNull
+    @Schema(example = "50000")
     private BigDecimal monthlyPayment;
 
     @NotNull
+    @Schema(example = "12.5")
     private BigDecimal rate;
 
     @NotNull
+    @Schema(example = "true")
     private Boolean isInsuranceEnabled;
 
     @NotNull
+    @Schema(example = "true")
     private Boolean isSalaryClient;
 }
