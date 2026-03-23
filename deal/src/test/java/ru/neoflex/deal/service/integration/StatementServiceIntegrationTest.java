6c5aad85c16312ff2b8d97ec4bc7c41f9cdf4811
bivansir <bivansir@gmail.com>
2026-03-23 21:05:01 +0300

    refactor: refactor imports

--- deal/src/test/java/ru/neoflex/deal/service/integration/StatementServiceIntegrationTest.java
+++ deal/src/test/java/ru/neoflex/deal/service/integration/StatementServiceIntegrationTest.java
@@ -21,7 +21,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DirtiesContext(
         classMode = DirtiesContext.ClassMode.AFTER_CLASS
