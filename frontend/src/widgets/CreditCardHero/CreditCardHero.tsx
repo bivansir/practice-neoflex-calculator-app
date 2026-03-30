f7a4c4093fcb7edd290be48b9b5479ec97af0dd8
bivansir <bivansir@gmail.com>
2026-03-30 19:45:36 +0300

    feature: creditcardhero

---
+++ frontend/src/widgets/CreditCardHero/CreditCardHero.tsx
@@ -0,0 +1,52 @@
+import { Button } from "../../shared/Button/Button";
+import './CreditCardHero.css';
+
+
+export const CreditCardHero = () => {
+    return (
+        <div className="credit-card-hero">
+            <div className="credit-card-hero__content">
+                <h1 className="credit-card-hero__title">
+                    Platinum digital credit card
+                </h1>
+                <div>
+                    <p className="credit-card-hero__description">
+                        Our best credit card. Suitable for everyday spending and shopping.
+                        Cash withdrawals and transfers without commission and interest.
+                    </p>
+                    
+                    <div className="credit-card-hero__subtitle-container">
+                        <div>
+                            <h3 className="credit-card-hero__subtitle">
+                                Up to 160 days
+                            </h3>
+                            <p className="credit-card-hero__text">
+                                No percent
+                            </p>
+                        </div>
+                        <div>
+                            <h3 className="credit-card-hero__subtitle">
+                                Up to 600 000 ₽
+                            </h3>
+                            <p className="credit-card-hero__text">
+                                Credit limit
+                            </p>
+                        </div>
+                        <div>
+                            <h3 className="credit-card-hero__subtitle">
+                                0 ₽
+                            </h3>
+                            <p className="credit-card-hero__text">
+                                Card service is free
+                            </p>
+                        </div>
+                    </div>
+                    <Button name='Apply for Card'></Button>
+                </div>
+            </div>
+            <div className="credit-card-hero__image-container">
+                <img src="/src/assets/images/credit-card.png" alt="credit-card" className="credit-card-hero__image" />
+            </div>
+        </div>
+    )
+}
