0d4e3145aea7269b9e527a3630fcef2b7418b26f
bivansir <bivansir@gmail.com>
2026-04-18 17:02:57 +0300

    refactor: infosection to tabs

--- frontend/src/widgets/InfoSection/InfoSection.tsx
+++ frontend/src/widgets/InfoSection/InfoSection.tsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import { InfoSectionMenu } from "./InfoSectionMenu";
 import { About } from "./About/About";
 import { Cashback } from "./Cashback/Cashback";
 import { FAQ } from "./FAQ/FAQ";
 import './InfoSection.css';
 import { RatesAndConditions } from "./RatesAndConditions/RatesAndConditions";
+import { Tabs } from "@/shared/Tabs/Tabs";
 
 export const InfoSection = () => {
-    const [activeId, setActiveId] = useState(1);
     return(
         <section className='info-section'>
-            <InfoSectionMenu activeId={activeId} setActiveId={setActiveId} />
-            {activeId === 1 && <About />}
-            {activeId === 2 && <RatesAndConditions />}
-            {activeId === 3 && <Cashback />}
-            {activeId === 4 && <FAQ />}
+            <Tabs items={[
+                { id: 1, label: "About", content: <About /> },
+                { id: 2, label: "Rates and Conditions", content: <RatesAndConditions /> },
+                { id: 3, label: "Cashback", content: <Cashback /> },
+                { id: 4, label: "FAQ", content: <FAQ /> }
+            ]} defaultActiveId={1} />
         </section>
     )
 }
