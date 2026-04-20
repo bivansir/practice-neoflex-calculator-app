2e453bad1c9f1be77c889b874f013a05681e93b4
bivansir <bivansir@gmail.com>
2026-04-20 18:30:32 +0300

    fix: info section

--- frontend/src/widgets/InfoSection/InfoSection.tsx
+++ frontend/src/widgets/InfoSection/InfoSection.tsx
@@ -1,12 +1,11 @@
 import { About } from "./About/About";
 import { Cashback } from "./Cashback/Cashback";
 import { FAQ } from "./FAQ/FAQ";
-import './InfoSection.css';
 import { RatesAndConditions } from "./RatesAndConditions/RatesAndConditions";
 import { Tabs, type TabItem } from "@/shared/Tabs/Tabs";
 
 const tabItems: TabItem[] = [
-    { id: 1, label: "About", content: <About /> },
+    { id: 1, label: "About card", content: <About /> },
     { id: 2, label: "Rates and Conditions", content: <RatesAndConditions /> },
     { id: 3, label: "Cashback", content: <Cashback /> },
     { id: 4, label: "FAQ", content: <FAQ /> }
