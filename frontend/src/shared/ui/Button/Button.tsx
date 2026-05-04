70295602c5a8c4bb85fd5942b2e4646d91763620
bivansir <bivansir@gmail.com>
2026-05-05 01:20:34 +0300

    refactor: Button

--- frontend/src/shared/ui/Button/Button.tsx
+++ frontend/src/shared/ui/Button/Button.tsx
@@ -4,9 +4,11 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     name?: string;
 };
 
-export const Button = ({ name, className }: ButtonProps) => {
+export const Button = ({ name, className, ...props}: ButtonProps) => {
     return (
-        <button className={`button ${className || ''} text text--spaced text--tight`}>
+        <button 
+        className={`button ${className || ''} text text--spaced text--tight`}
+        {...props}>
             {name}
         </button>
     )
