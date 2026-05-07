c46669be51ee3fabdbe201971968a8d802cdf107
bivansir <bivansir@gmail.com>
2026-05-07 17:06:02 +0300

    feature: CheckBox

--- frontend/src/shared/ui/Checkbox/Checkbox.tsx
+++ frontend/src/shared/ui/Checkbox/Checkbox.tsx
@@ -1,17 +1,19 @@
-import './checbox.css'
+import './checkbox.css'
 
 
 type CheckboxProps = {
     name: string
     label: string
+    onCheck: () => void
 }
 
-export const Checkbox = ( { name, label }: CheckboxProps) => {
+export const Checkbox = ( { name, label, onCheck }: CheckboxProps) => {
     return (
         <div className='checkbox'>
             <input
             id={name}
-            type='checkbox'/>
+            type='checkbox'
+            onChange={() => onCheck()}/>
             <label htmlFor={name}>
                 {label}
             </label>
