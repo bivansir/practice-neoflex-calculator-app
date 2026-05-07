251a9f459f0e42c124a4045acff711872590bd66
bivansir <bivansir@gmail.com>
2026-05-07 17:07:50 +0300

    fix: imports

--- frontend/src/pages/LoanPage/ui/LoanPage.tsx
+++ frontend/src/pages/LoanPage/ui/LoanPage.tsx
@@ -2,7 +2,7 @@ import { CreditCardHero } from "@/pages/LoanPage/ui/CreditCardHero/CreditCardHer
 import { HowToBanner } from "@/pages/LoanPage/ui/HowToBanner/HowToBanner"
 import { InfoSection } from "@/pages/LoanPage/ui/InfoSection/InfoSection"
 import { useApplicationStore } from "@/entities/application/store"
-import { LoanFirstStep } from "../../../features/application/LoanFirstStep/LoanFirstStep"
+import { LoanFirstStep } from "../../../features/application/ui/LoanFirstStep/LoanFirstStep"
 
 export const LoanPage = () => {
     const step = useApplicationStore((s) => s.step);
